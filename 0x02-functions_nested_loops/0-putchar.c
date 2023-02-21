e3af38b0d7d0e7c7aa4f89cf41ffaf3b0916ddd1
devnabibia <nangendodennis@gmail.com>
2023-02-21 10:32:43 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * main - prints the string "putchar" from a character array.
+ * main - prints the string "putchar" from a character array on a new line.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
+    char c[] = "_putchar";
+    int i;
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	putchar('\n')
-	return (0);
+    for (i = 0; i < 8; i++)  // print the first 8 characters of the string
+    {
+        _putchar(c[i]);
+    }
+
+    _putchar('\n');  // print a newline character on a separate line
+
+    return (0);
 }
+
