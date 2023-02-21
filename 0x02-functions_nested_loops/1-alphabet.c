8116d9d8efc92efa863f1375e097210d82bd117d
devnabibia <nangendodennis@gmail.com>
2023-02-21 03:08:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
