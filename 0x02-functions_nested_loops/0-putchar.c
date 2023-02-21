c975eaa28b9c63efa23db85aa333bba3160d5ee6
devnabibia <nangendodennis@gmail.com>
2023-02-21 09:53:07 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints the string "_putchar" from a character array.
+ * main - prints the string "putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
@@ -9,10 +9,10 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 9; i++)
-	{
+
 		_putchar(c[i]);
-	}
-	_putchar('\n');
+	
+        _putchar('\n');
 	return (0);
 }
 
