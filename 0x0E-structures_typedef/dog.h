e0216d3078556047bfffd2184710931dbc77bb2d
devnabibia <nangendodennis@gmail.com>
2023-03-20 11:05:18 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,61 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog dog_t;
 
 /**
- * struct dog - Short description
- * @name: firts member
- * @age: second member
- * @owner: third member
- *
- * Description: Longer description
+ * struct dog - Dog with a few attributes
+ * @name: The name of the dog as a pointer to the first character of a string
+ * @age: The age of the dog aas a float
+ * @owner: The name of the owner of the dog as a pointer to the first character
+ * of a string
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-/**
-  * _putchar - print a character
-  * @c: character.
-  * Return: c
-  */
-int _putchar(char c); /*prototype _putchar*/
-
-/*function that initialize a variable of type struct dog*/
-/**
-  * init_dog - print a character
-  * @d: character.
-  *@name: char
-  *@age: int
-  *@owner: char
-  * Return: c
-  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/*function that initialize a variable of type struct dog*/
-/**
-  * print_dog - print a character
-  * @d: character.
-  * Return: c
-  */
-void print_dog(struct dog *d);/*function that prints a struct dog*/
-
-/**
-  * new_dog - print a character
-  * @name: firts member
-  * @age: second member
-  * @owner: third member
-  * Description: Longer description
-  *Return: Always 0.
-  */
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
-/**
-  * free_dog - print a character
-  * @d: firts member
-  * Description: Longer description
-  */
+char *dup_strint(char *s);
 void free_dog(dog_t *d);
 
 #endif
