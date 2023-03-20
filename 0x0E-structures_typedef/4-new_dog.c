accfd2cd9a09c243a8d223910d1648905ec240cb
devnabibia <nangendodennis@gmail.com>
2023-03-20 11:37:21 -0700

    thank you God

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,89 +1,83 @@
 #include "dog.h"
 #include <stdlib.h>
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
- * dup_string - Duplicates a string passed into a newly allocated space in
- * memory
- * @s: Pointer to the first character in the source string
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
  *
- * Return: Pointer to the first character in the newly allocated and copied
- * string
+ * Return: The length of the string.
  */
-char *dup_string(char *s)
+int _strlen(char *str)
 {
-	int len, i;
-	char *ret;
+	int len = 0;
 
-	for (len = 0; s[len]; len++)
-		;
+	while (*str++)
+		len++;
 
-	ret = malloc(sizeof(char) * (len + 1));
-	if (ret == NULL)
-		return (NULL);
-
-	for (i = 0; s[i]; i++)
-		ret[i] = s[i];
-	ret[i] = '\0';
-
-	return (ret);
+	return (len);
 }
 
 /**
- * new_dog - Makes a new dog with the given parameters
- * @name: Pointer to the first character of the string of the name of the dog
- * to be created
- * @age: Age of the dog to be created as a float
- * @owner: Pointer to the first character of the string of the owner of the dog
- * to be created
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
  *
- * Return: Pointer to the new `dog_t` with all of the given attributes
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *tname, *towner;
-	dog_t *ret;
+	dog_t *doggo;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-	ret = malloc(sizeof(dog_t));
-	if (ret == NULL)
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
 		return (NULL);
-	ret->age = age;
 
-	tname = dup_string(name);
-	if (tname == NULL)
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
 	{
-		free_dog(ret);
+		free(doggo);
 		return (NULL);
 	}
-	ret->name = tname;
 
-	towner = dup_string(owner);
-	if (towner == NULL)
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
 	{
-		free_dog(ret);
+		free(doggo->name);
+		free(doggo);
 		return (NULL);
 	}
-	ret->owner = towner;
 
-	return (ret);
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+
+	return (doggo);
 }
-
-/**
- * free_dog - Frees all memory allocated by a dog_t struct
- * @d: Pointer to the struct to be freed
- */
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-		return;
-
-	if (d->name != NULL)
-		free(d->name);
-
-	if (d->owner != NULL)
-		free(d->owner);
-
-	free(d);
-
