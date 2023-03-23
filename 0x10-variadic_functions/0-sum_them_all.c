b2130a76ae0346c7b36adec7e95e2b6554091bf3
devnabibia <nangendodennis@gmail.com>
2023-03-23 06:43:11 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,27 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
  * sum_them_all - Entry Point
  * @n: variadic inputs
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	va_list args;
+	va_list valist;
+	unsigned int i, sum = 0;
 
-	va_start(args, n);
+	if (n == 0)
+		return (0);
 
-	for (unsigned int i = 0; i < n; i++)
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(valist, const unsigned int);
 	}
-	va_end(args);
+
+	va_end(valist);
+
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,28 +1,28 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
 /**
  * print_numbers - Entry Point
  * @separator: comma space
  * @n: elements to be printed
  * Return: void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	char *sep;
+	unsigned int i;
+	va_list list;
 
-	va_start(args, n);
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
+	va_start(list, n);
 
-	for (unsigned int i = 0; i < n; i++)
-	{
-		printf("%d", va_arg(args, int));
-		if (separator != NULL && i < n - 1)
-		{
-			printf("%s", separator);
-		}
-	}
-	va_end(args);
+	if (n > 0)
+		printf("%d", va_arg(list, int));
+	for (i = 1; i < n; i++)
+		printf("%s%d", sep, va_arg(list, int));
 	printf("\n");
+	va_end(list);
 }
-
