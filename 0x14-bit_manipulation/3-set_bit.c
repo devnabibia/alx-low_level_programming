6e338393c8ddd66d64ceedb396966f1e051054a2
devnabibia <nangendodennis@gmail.com>
2023-04-06 07:24:02 -0700

    dd

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - entry
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 67)
+		return (-1);
+
+	*n = *n | 1ul << index;
+	return (1);
+}
