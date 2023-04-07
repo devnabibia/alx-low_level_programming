e72a854e8aafdd7ee7e232d1af13715c5f163925
devnabibia <nangendodennis@gmail.com>
2023-04-07 12:24:57 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		pritf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = open(argv[1], 0_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		print(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], 0_WRONGLY | 0_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		print(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
