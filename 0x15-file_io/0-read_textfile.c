0147496bcd76303427dc5d03fb160cd1a3ee3b5b
devnabibia <nangendodennis@gmail.com>
2023-04-07 11:31:41 -0700

    gfgfg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,41 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include "main.h"
 
 /**
- * read_textfile - entry point
- * @filename: char
- * @letters: int
- * Return: 0
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t file, dee, x;
-	char *dx;
+	ssize_t file, let, w;
+	char *text;
 
-	dx = malloc(letters);
-	if (dx == NULL)
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, 0_RDONLY);
+	file = open(filename, O_RDONLY);
 
 	if (file == -1)
 	{
-		free(dx);
+		free(text);
 		return (0);
 	}
 
-	dee = read(file, dx, letters);
-	x = write(STDOUT_FILENO, dx, dee);
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
 
 	close(file);
 
-	return (x);
+	return (w);
 }
